7795d9f6f7ad4e9cd95f50404215ac73563fde22
niehusst <liamniehusstaab@gmail.com>
2021-05-19 22:55:43 -0600

    chore: prepare script for test imporation

--- src/mopbx.py
+++ src/mopbx.py
@@ -11,5 +11,8 @@ TODO: use command line flags to specify testing mode and/or override pbx path
 # hard-code the path to your pbxproj file here for easier use
 pbx_path = ""
 
-if __name__ == "__main__":
+def main():
     pass
+
+if __name__ == "__main__":
+    main()
