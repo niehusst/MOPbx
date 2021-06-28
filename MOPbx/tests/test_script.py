5e12cb2110be03b06b578c091836b99d6b13448a
niehusst <liamniehusstaab@gmail.com>
2021-06-27 19:25:48 -0600

    feat: fill in expected test result data

--- MOPbx/tests/test_script.py
+++ MOPbx/tests/test_script.py
@@ -34,7 +34,7 @@ last_ref_pbx = "../tests/data/DanglingRefs/ExampleProj.xcodeproj/project.pbxproj
 #>* remove .strings files from fs where source xib (or storyboard) not present in fs (and pbx)
 def test_all_translation_files_without_source_removed():
     res = remove_translation_files_without_source(no_layout_proj, no_layout_pbx, test_mode)
-    assert res == ["TODO"], f"List of files to remove dont match. {no_layout_proj}"
+    assert res == ["ExampleProj/es.lproj/Main.strings"], f"List of files to remove dont match. {no_layout_proj}"
 
 
 #>* clean pbx/fs not changed
@@ -47,18 +47,18 @@ def test_clean_pbx_valid_project_with_no_missing_files():
 #>* all references are correctly removed completely from pbx, including translation language from xib set (file comparison to manually fix?)
 def test_clean_pbx_invalid_project_with_missing_files():
     res = clean_pbx(danlging_refs_proj, danlging_refs_pbx, test_mode)
-    assert res == ["TODO"], f"List of refs did not contain expect refs. {danlging_refs_proj}"
+    expected = ["ExampleProj/DetailViewController.swift", "ExampleProj/DetailViewController.xib", "ExampleProj/Content/pic4.png", "ExampleProj/Base.lproj/LaunchScreen.storyboard", "ExampleProj/es.lproj/Main.strings"]
+    assert res == expected, f"List of refs did not contain expect refs. {danlging_refs_proj}"
 
 
 #>* empty .strings files removed from file system
 #>* fully empty .strings files + ones that have 1 space in it
 def test_remove_empty_translation_files():
     res = remove_empty_translation_files(empty_strings_proj, empty_string_pbx, test_mode)
-    assert res == ["TODO"], f"List of files to remove dont match. {empty_strings_proj}"
+    assert res == ["ExampleProj/es.lproj/Main.strings", "ExampleProj/es.lproj/LaunchScreen.strings"], f"List of files to remove dont match. {empty_strings_proj}"
 
 
 #>* removing a ref from pbx that is last element in array doesnt break compile (prev elem now has trailing comma)
 def test_last_ref_removal_compiles():
-    res = clean_pbx(last_ref_proj, last_ref_pbx, test_mode)
-    assert res == ["TODO"], f"List of files to remove dont match. {last_ref_proj}"
-    #TODO test proj compiles???
+    clean_pbx(last_ref_proj, last_ref_pbx, test_mode)
+    #TODO test proj compiles?? compare to manual fix file?
